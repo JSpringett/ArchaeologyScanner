15b11bb4f986ed918a44f3ef1904b039e806a86b
Jordan Less'ard-Springett <jordan80100@gmail.com>
2015-12-29 22:02:23 +0000

    FIxed David's mega cockup

--- laser-detector.py
+++ laser-detector.py
@@ -17,16 +17,18 @@ ser = serial.Serial('/dev/tty.usbmodem1421', 19200)
 
 while(True):
     ret, frame = cap.read();
+    frame = cv2.line(frame, (0, int(cap.get(4)) - 100), (int(cap.get(3)), int(cap.get(4)) - 100), (0,0,255),5)  
+    frame = cv2.line(frame, (int(cap.get(3) / 2), 0), (int(cap.get(3) / 2), int(cap.get(4))), (255,0,0),1)  
     cv2.imshow("Initial", frame)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break;
+        break
 cv2.destroyAllWindows()
 
 
 for k in range(160):
     print k + 10
     ser.write(str(k + 10))
+    time.sleep(0.5)
 
     line = []
     ret, frame = cap.read()
@@ -36,7 +38,7 @@ for k in range(160):
     # Display
 
     height, width = thresh.shape
-    for i in range(height):
+    for i in range(height - 100):
         for j in range(width):
             #if j == width:
 
@@ -48,13 +50,13 @@ for k in range(160):
                 x, y = pol2cart((width/2 - j), np.radians(k))
                 z = i
                 target.write("v ")
-                target.write(str(x/float(100)))
+                target.write(str(x/float(100))
                 target.write(" ")
-                target.write(str(y/float(100)))
+                target.write(str(y/float(100))
                 target.write(" ")
-                target.write(str(z/float(100)))
+                target.write(str(z/float(100))
                 target.write("\n")
-                #cv2.imshow('jhgjh', thresh)
+                cv2.imshow('jhgjh', thresh)
 
 
                 break
